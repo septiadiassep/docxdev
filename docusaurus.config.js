65db6d1387077fdef9b5077456070b2b03175d74
septiadiassep <septiadiassep@gmail.com>
2025-06-13 00:50:22 +0700

    Add: module ci/cd GitHub Action auto deploy

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -20,18 +20,19 @@ const config = {
   },
 
   // Set the production url of your site here
-  url: 'https://your-docusaurus-site.example.com',
+  url: 'https://septiadiassep.github.io',
+  trailingSlash: false,
   // Set the /<baseUrl>/ pathname under which your site is served
   // For GitHub pages deployment, it is often '/<projectName>/'
-  baseUrl: '/',
+  baseUrl: '/docxdev/',
 
   // GitHub pages deployment config.
   // If you aren't using GitHub pages, you don't need these.
-  organizationName: 'facebook', // Usually your GitHub org/user name.
-  projectName: 'docusaurus', // Usually your repo name.
+  organizationName: 'septiadiassep', // Usually your GitHub org/user name.
+  projectName: 'docxdev', // Usually your repo name.
 
   onBrokenLinks: 'throw',
-  onBrokenMarkdownLinks: 'warn',
+  onBrokenMarkdownLinks: 'throw',
 
   // Even if you don't use internationalization, you can use this field to set
   // useful metadata like html lang. For example, if your site is Chinese, you
